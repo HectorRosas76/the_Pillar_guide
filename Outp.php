58540f3a1713e5a588d419a40ca24eeee351668e
HectorRosas76 <hecrosas@uniboyaca.edu.co>
2026-03-18 14:53:29 -0500

    actualizar

--- Outp.php
+++ Outp.php
@@ -1,46 +1,50 @@
 <?php
-echo 'hola mundo'
-class Figura{
-    public abstract function area();/*polimorfismo*/
+echo 'hola mundo';
+
+abstract class Figura{
+    public abstract function area();
+
     public function getAreaMsg(){
-return "\n El area es de". $this->area();
+        return "\n El area es de " . $this->area();
     }
 }
 
 interface IFigura{
-public function toString();
+    public function toString();
 }
 
-class Cuadrado extends Figura implements IFigura
-{
+class Cuadrado extends Figura implements IFigura{
+
     private $arista;
-    public function __construc($arista){
+
+    public function __construct($arista){
         $this->arista = $arista;
-    } 
+    }
+
     public function setArista($value){
-        $this->arista= $value;
+        $this->arista = $value;
     }
+
     public function getArista(){
-        return $this->arista;/*herencia*/
+        return $this->arista;
     }
+
     public function area(){
         return pow($this->arista, 2);
     }
-    /* pow= funcion*/
 
-    public function toString(){/*polimorfismo*/
-        return "\n El cuadrado es de". $this->area();
+    public function toString(){
+        return "\n El cuadrado es de " . $this->arista;
     }
-    }
+}
 
+class Rectangulo extends Figura implements IFigura{
 
-class Rectangulo{
     private $base = 0;
     private $altura = 0;
 
-    public function __construct($base, $altura)
-    {
-       $this->base = $base;
+    public function __construct($base, $altura){
+        $this->base = $base;
         $this->altura = $altura;
     }
 
@@ -49,42 +53,37 @@ class Rectangulo{
     }
 
     public function toString(){
-        return "\n El rectangulo es de ". $this->base. "x" $this->altura;
+        return "\n El rectangulo es de " . $this->base . "x" . $this->altura;
     }
 }
 
-
-$cuadrado1 = new Cuadrado(25);
-echo "\n El cuadrado es de". $cuadrado1->arista;
-$cuadrado1 = new Cuadrado(25);
-echo "\n El cuadrado es de". $cuadrado1->getArista();
-$cuadrado1 = new Cuadrado(25);
-echo "\n El area es de". $cuadrado1->area();
-
-$rectangulo1 = new Rectangulo(10, 25);
-echo $rectagulo1= toString();
-echo $rectagulo1= getAreaMsg();
-
-
 class Triangulo extends Figura implements IFigura{
-private $base = 0;
-private $altura = 0;
 
-public function __construct($base,$altura)
- {
-       $this->base = $base;
+    private $base = 0;
+    private $altura = 0;
+
+    public function __construct($base,$altura){
+        $this->base = $base;
         $this->altura = $altura;
     }
 
-        public function area(){
-        return $this->base * $this->altura;
+    public function area(){
+        return ($this->base * $this->altura) / 2;
     }
 
     public function toString(){
-        return "\n El triangulo es de ". $this->base. "x" $this->altura;
+        return "\n El triangulo es de " . $this->base . "x" . $this->altura;
     }
-
-
 }
 
-?>
+$cuadrado1 = new Cuadrado(25);
+echo $cuadrado1->toString();
+echo $cuadrado1->getAreaMsg();
+
+$rectangulo1 = new Rectangulo(10,25);
+echo $rectangulo1->toString();
+echo $rectangulo1->getAreaMsg();
+
+$triangulo1 = new Triangulo(20,30);
+echo $triangulo1->toString();
+echo $triangulo1->getAreaMsg();
